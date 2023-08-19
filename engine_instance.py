bf00140fd776136fe5e45c130fdc841a2b3dbe58
TaimoorIkram <tikram.bese21seecs@seecs.edu.pk>
2023-08-19 13:20:58 +0500

    Add engine.py

---
+++ engine_instance.py
@@ -0,0 +1,22 @@
+import re
+from api.serializers import ProductSerializer
+from restbase.models import Product
+from .engine import SearchEngine
+import json
+
+def data_fetcher(pks):
+    ids = pks
+    products = []
+    for _id in ids:
+        product = Product.objects.get(id=_id)
+        products.append(product)
+    serializer = ProductSerializer(products, many=True)
+    data = serializer.data
+    data = [ dict(i) for i in data ]
+    return data
+
+
+engine = SearchEngine(
+    data_fetcher = data_fetcher,
+    dump_path = "utils",
+)
