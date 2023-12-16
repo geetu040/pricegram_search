1d8a54ab5cce8c7dcb0e92c7bb892039b2957a70
geetu040 <raoarmaghanshakir040@gmail.com>
2023-12-16 20:20:39 +0500

    adding empty test structure

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+import unittest
+from pricegram_search.utils import BasicUtils, Algorithms, ProductsMatch
+
+class TestUtils(unittest.TestCase):
+    def test_clean_zero(self):
+        # Write test cases for the clean_zero method in BasicUtils
+        pass
+
+    def test_matching_ratio(self):
+        # Write test cases for the matching_ratio method in Algorithms
+        pass
+
+    def test_part_keyword(self):
+        # Write test cases for the part_keyword method in ProductsMatch
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
